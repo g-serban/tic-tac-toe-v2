339a95bf3727103a53562c1177ab286ae6e581dd
Serban Gombos <serban.gombos@qubiz.com>
2021-05-24 20:10:07 +0300

    update

--- minimax-tictactoe-poe.py
+++ minimax-tictactoe-poe.py
@@ -21,11 +21,9 @@ def evaluate(b):
 
         if (b[row][0] == b[row][1]) and (b[row][1] == b[row][2]):
             if b[row][0] == player:
-
                 return 10
 
             elif b[row][0] == opponent:
-
                 return -10
 
     # Checking for Columns for X or O victory.
@@ -33,30 +31,24 @@ def evaluate(b):
 
         if (b[0][col] == b[1][col]) and (b[1][col] == b[2][col]):
             if b[0][col] == player:
-
                 return 10
 
             elif b[0][col] == opponent:
-
                 return -10
 
     # Checking for Diagonals for X or O victory.
     if (b[0][0] == b[1][1]) and (b[1][1] == b[2][2]):
         if b[0][0] == player or b[1][1] == player or b[2][2] == player:
-
             return 10
 
         elif b[0][0] == opponent or b[1][1] == opponent or b[2][2] == opponent:
-
             return -10
 
     elif (b[0][2] == b[1][1]) and (b[1][1] == b[2][0]):
         if b[0][2] == player or b[1][1] == player or b[0][2] == player:
-
             return 10
 
         elif b[0][2] == opponent or b[1][1] == opponent or b[0][2] == opponent:
-
             return -10
 
     # Else if none of them have won then return 0
@@ -135,9 +127,9 @@ def find_best_move(board):
 
 
 board = [
-    ['x', '_', 'o'],
-    ['_', '_', '_'],
-    ['o', '_', '_']
+    ['o', 'x', 'o'],
+    ['x', 'o', '_'],
+    ['x', 'x', 'x']
 ]
 
 best_move = find_best_move(board)
